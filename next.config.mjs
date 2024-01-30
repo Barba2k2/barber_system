8a7cd1fea44cad2f6d313527688800e14bc67646
Barba2k2 <lorenzo.dz@hotmail.com>
2024-01-30 09:38:09 -0300

    feat: add barbershop initial item

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
