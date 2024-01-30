b203d161ffb42ad58c1dd484908f08d2126a0334
Barba2k2 <lorenzo.dz@hotmail.com>
2024-01-29 23:12:51 -0300

    feat: add initial header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Header from "../_components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1>BarberShop - By Barba Tech</h1>
+    </>
+  );
+}
