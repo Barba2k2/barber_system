de41fb30445a31ecf67a3a995a075c5fb1681b70
Barba2k2 <lorenzo.dz@hotmail.com>
2024-01-31 14:28:01 -0300

    feat: add Servieitem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,21 +1,33 @@
+"use client";
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 
 const BarberhopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="w-full relative h-[159px]">
           <div className="absolute top-2 left-2 z-50">
-            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center absolute top-2 left-2">
+            <Badge
+              variant="secondary"
+              className="opacity-90 flex gap-1 items-center absolute top-2 left-2"
+            >
               <StarIcon size={12} className="fill-primary text-primary" />
               <span className="text-xs">5,0</span>
             </Badge>
@@ -36,7 +48,11 @@ const BarberhopItem = ({ barbershop }: BarbershopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {barbershop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">
+          <Button
+            className="w-full mt-3"
+            variant="secondary"
+            onClick={handleBookingClick}
+          >
             Reservar
           </Button>
         </div>
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,19 +4,21 @@ import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
-import BarberhopItem from "./_components/barbershop-item";
+import BarbershopItem from "./_components/barbershop-item";
 
 export default async function Home() {
-  const barbershop = await db.barbershop.findMany({});
+  const barbershops = await db.barbershop.findMany({});
 
   return (
-    <>
+    <div>
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Barba!</h2>
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">
-          {format(new Date(), "EEE',' dd 'de' MMMM", { locale: ptBR })}
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
         </p>
       </div>
 
@@ -25,35 +27,35 @@ export default async function Home() {
       </div>
 
       <div className="px-5 mt-6">
-        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
           Agendamentos
         </h2>
         <BookingItem />
       </div>
 
-      <div className=" mt-6">
-        <h2 className="text-xs px-5 uppercase text-gray-400 font-bold mb-3">
+      <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Recomendados
         </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershop.map((barbershop) => (
-            <BarberhopItem key={barbershop.id} barbershop={barbershop} />
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
 
-      <div className=" mt-6 mb-20">
-        <h2 className="text-xs px-5 uppercase text-gray-400 font-bold mb-3">
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Populares
         </h2>
 
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershop.map((barbershop) => (
-            <BarberhopItem key={barbershop.id} barbershop={barbershop} />
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
