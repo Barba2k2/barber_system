2d3335ad43d77652e0408e6beaa0aedde0774fb9
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-01 14:18:58 -0300

    feat: add initial service booking

---
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -0,0 +1,17 @@
+import { addMinutes, format, setHours, setMinutes } from "date-fns";
+
+export function generateDayTimeFromList(date: Date): string[] {
+  const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 9 AM
+  const endTime = setMinutes(setHours(date, 18), 0); // Set end time to 6 PM
+  const interval = 60; // Interval in minutes
+  const timeList: string[] = [];
+
+  let currentTime = startTime;
+
+  while (currentTime <= endTime) {
+    timeList.push(format(currentTime, "HH:mm"));
+    currentTime = addMinutes(currentTime, interval);
+  }
+
+  return timeList;
+}
