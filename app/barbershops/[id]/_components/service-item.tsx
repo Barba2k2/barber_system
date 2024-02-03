500784e0599c678648d503cfb6b83f1ce8efa727
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-03 12:36:30 -0300

    fix: ensure first avaliable day to book a service is tomorrow

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -17,8 +17,8 @@ import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useEffect, useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { format, setHours, setMinutes } from "date-fns";
-import { saveBooking } from '../_actions/save-booking'
+import { addDays, format, setHours, setMinutes } from "date-fns";
+import { saveBooking } from "../_actions/save-booking";
 import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
@@ -179,7 +179,7 @@ const ServiceItem = ({
                       selected={date}
                       onSelect={handleDateClick}
                       locale={ptBR}
-                      fromDate={new Date()}
+                      fromDate={addDays(new Date(), 1)}
                       styles={{
                         head_cell: {
                           width: "100%",
--- app/barbershops/[id]/_helpers/hours.ts
+++ app/barbershops/[id]/_helpers/hours.ts
@@ -2,8 +2,8 @@ import { setHours, setMinutes, format, addMinutes } from "date-fns";
 
 export function generateDayTimeList(date: Date): string[] {
   const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 09:00
-  const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
-  const interval = 45; // interval in minutes
+  const endTime = setMinutes(setHours(date, 17), 0); // Set end time to 21:00
+  const interval = 60; // interval in minutes
   const timeList: string[] = [];
 
   let currentTime = startTime;
